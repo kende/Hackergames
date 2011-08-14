bdee861b88a6ba86e9f602ebf8af1aa4b693b7ae
kende <kende.j@gmail.com>
2011-08-14 16:21:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackergames::Application.config.secret_token = 'f89a354268f20dc5399c065fe8b6627427585eacfd84de2fcf46b0fe00e8fb940c5316a41d10f40a3959f7a72fd9a1427c2904b2b80fdf28520459892cf70241'
